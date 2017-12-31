cb5e454ed106ce4ed394865ac2985bb5e149eb56
Hikaru Chang <hikaru@joymoe.com>
2017-12-31 12:59:33 +0800

    Pre release

---
+++ config.json.js
@@ -0,0 +1,9 @@
+{
+    "build_date": "2017-12-43_12:43:18", 
+    "description": "UDP双边加速工具", 
+    "home_url": "Module_udpspeeder.asp", 
+    "md5": "ab2338b5e10c5086d0cc1072c38eed76", 
+    "module": "udpspeeder", 
+    "title": "udpspeeder", 
+    "version": "2.0.1"
+}
