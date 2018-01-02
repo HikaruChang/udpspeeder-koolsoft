7ec70e66c6fbd158ec9c8da1dcb0ea8f5677074d
Hikaru Chang <hikaru@joymoe.com>
2018-01-02 16:28:43 +0800

    Fix bug

--- config.json.js
+++ config.json.js
@@ -2,8 +2,8 @@
     "build_date": "2017-12-31_12:43:18", 
     "description": "UDP双边加速工具", 
     "home_url": "Module_udpspeeder.asp", 
-    "md5": "cb537c509bb98bb7a548bcc7031b10a9", 
+    "md5": "7f583ccb23862e1231f250b25b78e5e4", 
     "module": "udpspeeder", 
     "title": "udpspeeder", 
-    "version": "2.0.1"
+    "version": "2.0.1b"
 }
